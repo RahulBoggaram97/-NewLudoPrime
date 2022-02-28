aac74f827c5cd2136e76ec6978a9310eee0a4aac
Ahlawat23 <66788786+Ahlawat23@users.noreply.github.com>
2022-02-28 15:58:06 +0530

    k

--- Assets/scripts/mainGameScripts/flutterInterigation/flutterManager.cs
+++ Assets/scripts/mainGameScripts/flutterInterigation/flutterManager.cs
@@ -5,21 +5,70 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using System;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
 
 namespace com.impactionalGames.LudoPrime
 {
     public class flutterManager : MonoBehaviour, IEventSystemHandler
     {
         public string levelToLoadAfterAuthentiocation;
+        public string refrelCode;
+
 
         private void Start()
         {
-            /*loadUpNextScene()*/;
+            //loadUpNextScene();
         }
 
-        public void passPhoneNumberToUnity(String phoneNum)
+        public void passPhoneNumberToUnity(String phoneNum, String _refrelCode)
         {
             playerPermData.setPhoneNumber(phoneNum);
+            refrelCode = _refrelCode;
+
+            createUser();
+
+            
+        }
+
+        
+
+        public void createUser() => StartCoroutine(createNewUser_Coroutine());
+
+
+
+        IEnumerator createNewUser_Coroutine()
+        {
+            
+
+            string url = "https://ludogame-backend.herokuapp.com/api/createUser";
+            WWWForm form = new WWWForm();
+            form.AddField("Phone", playerPermData.getPhoneNumber());
+            form.AddField("Referrer", refrelCode);
+
+            using (UnityWebRequest request = UnityWebRequest.Post(url, form))
+            {
+                yield return request.SendWebRequest();
+                if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+                {
+                   
+
+                    if (request.downloadHandler.text.Contains("Duplicate entry"))
+                    {
+
+
+
+
+                        loadUpNextScene();
+                    }
+                }
+                else
+                {
+
+
+                    loadUpNextScene();
+
+                }
+            }
         }
 
         public void loadUpNextScene()
