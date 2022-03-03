c40b57921680dfe2d1c328c8675c871d33e178e5
RahulBoggaram97 <rahulboggaram913@gmail.com>
2022-03-03 19:34:21 +0530

    LobbyChangeUi

--- Assets/scripts/OpenPanel.cs
+++ Assets/scripts/OpenPanel.cs
@@ -6,10 +6,18 @@ using UnityEngine.UI;
 public class OpenPanel : MonoBehaviour
 {
     public GameObject Panel;
+    public GameObject text;
 
     public void PanelOpen()
     {
         if(Panel !=null)
+        {
+            Panel.SetActive(true);
+        }
+    }
+    public void textOpen()
+    {
+        if (Panel != null)
         {
             Panel.SetActive(true);
         }
